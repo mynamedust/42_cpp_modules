dc4707ee64e9bbf1489388d913fb177ec183b089
almelikyan <almelikyan@ozon.ru>
2024-07-12 21:10:20 +0400

    [cpp_module_08] add Span class

---
+++ cpp_module_08/ex01/Span.cpp
@@ -0,0 +1,59 @@
+#include "Span.hpp"
+
+Span::Span() : _cap(0) {
+	std::cout << "Default span constructor called. Capacity: 0" << std::endl;
+}
+
+Span::Span(const unsigned int cap) : _cap(cap) {
+	std::cout << "Param span constructor called. Capacity: " << cap << std::endl;
+}
+
+Span::Span(const Span& other) {
+	_cap = other._cap;
+	_arr = other._arr;
+	std::cout << "Copy span constructor called. Capacity: " << _cap << std::endl;
+}
+
+Span& Span::operator=(const Span& other) {
+	if (this != &other) {
+		_cap = other._cap;
+		_arr = other._arr;
+	}
+	return *this;
+}
+
+void Span::addNubmer(int value) {
+	if (_arr.size() >= _cap)
+		throw SpanOverflow();
+	_arr.push_back(value);
+}
+
+int Span::longestSpan() const {
+	if (_arr.size() <= 2)
+		throw NotEnoughElem();
+	return *std::max_element(_arr.begin(), _arr.end()) - *std::min_element(_arr.begin(), _arr.end());
+}
+
+int Span::shortestSpan() const {
+	if (_arr.size() <= 2)
+		throw NotEnoughElem();
+	std::vector<int> tmpArr = _arr;
+	std::sort(tmpArr.begin(), tmpArr.end());
+
+	int span = tmpArr[1] - tmpArr[0];
+	for (size_t i = 2; i < tmpArr.size(); i++)
+		span = std::min(span, tmpArr[i] - tmpArr[i - 1]);
+
+	return span;
+}
+
+const char *Span::NotEnoughElem::what() const throw() {
+	return "Not enough elements. Need minimum 2." << std::endl;
+}
+
+const char *Span::SpanOverflow::what() const throw()
+{
+	return "Span already filled.";
+}
+
+
---
+++ cpp_module_08/ex01/Span.hpp
@@ -0,0 +1,33 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+class Span {
+	std::vector<int> _arr;
+	unsigned int _cap;
+public:
+	Span();
+	Span(const unsigned int cap);
+	Span(const Span& other);
+	Span& operator=(const Span& other);
+
+
+	void addNubmer(int value);
+	int shortestSpan() const;
+	int longestSpan() const;
+
+	class SpanOverflow : public std::exception {
+		const char *what() const throw;
+	};
+
+	class NotEnoughElem : public std::exception {
+		const char *what() const throw;
+	};
+};
+
+
+
+#endif //SPAN_HPP
