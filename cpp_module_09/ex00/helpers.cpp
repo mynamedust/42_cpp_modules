c77781c0bb72e3d29ae4cae19612783fa9c0376b
mynamedust <mynamedust@gmail.com>
2024-10-10 21:04:17 +0400

    [cpp_module_09] fix ex00

---
+++ cpp_module_09/ex00/helpers.cpp
@@ -0,0 +1,41 @@
+#include <ctime>
+#include <iostream>
+#include "BitcoinExchange.hpp"
+
+int compareDates(int year1, int month1, int day1, int year2, int month2, int day2) {
+	if (year1 < year2) {
+		return -1;
+	} else if (year1 > year2) {
+		return 1;
+	} else {
+		if (month1 < month2) {
+			return -1;
+		} else if (month1 > month2) {
+			return 1;
+		} else {
+			if (day1 < day2) {
+				return -1;
+			} else if (day1 > day2) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+}
+
+void getCurrentDate(int &year, int &month, int &day) {
+	std::time_t t = std::time(0);
+	std::tm* now = std::localtime(&t);
+	year = now->tm_year + 1900;
+	month = now->tm_mon + 1;
+	day = now->tm_mday;
+}
+
+void clearConsole() {
+#ifdef _WIN32
+	system("cls");
+#else
+	system("clear");
+#endif
+}
